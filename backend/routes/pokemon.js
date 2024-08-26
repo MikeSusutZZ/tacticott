ba5c15d43887fc2d984f971a9d87659934d29c92
MikeSusutZZ <mike.susut@gmail.com>
2024-08-26 00:05:14 -0700

    testing ssh

--- backend/routes/pokemon.js
+++ backend/routes/pokemon.js
@@ -11,4 +11,6 @@ router.get('/:name', pokemonController.getPokemonByName);
 // Add or update a Pokémon entry
 router.post('/', pokemonController.addOrUpdatePokemon);
 
+router.get('/test', async (req, res) => res.send('hello'))
+
 module.exports = router;
