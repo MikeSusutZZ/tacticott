4046610a9bcdb6d14dd75cf02eb06438e2a62f27
MikeSusutZZ <mike.susut@gmail.com>
2024-09-09 09:35:47 -0700

    Thank you screen aligned

--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import { Container, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Container, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function ThankYou() {
   return (
     <Container className="text-center my-5">
-      <h1>Thank You for Your Contribution!</h1>
-      <img src="https://s1.zerochan.net/Whimsicott.600.3753121.jpg" alt="Thank you" className="my-4" />
-      <Link to="/">
-        <Button variant="primary">Return Home</Button>
-      </Link>
+      <div className="d-flex flex-column align-items-center">
+        <h1>Thank You for Your Contribution!</h1>
+        <img
+          src="https://s1.zerochan.net/Whimsicott.600.3753121.jpg"
+          alt="Thank you"
+          className="my-4"
+        />
+        <Link to="/">
+          <Button variant="primary">Return Home</Button>
+        </Link>
+      </div>
     </Container>
   );
 }
